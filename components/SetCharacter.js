c8719094e16347bc4328316759270b1386a66c7b
Alex <alex.binenstock@gmail.com>
2019-01-21 18:16:20 -0600

    init

---
+++ components/SetCharacter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from 'react-native';
+
+const setCharacter = props => {
+    return(
+        <Button onPress={this.beginCharacterSelection} title={'Set Character'} />
+    );
+}
+
+beginCharacterSelection = () => {
+    return;
+}
+
+export default setCharacter;
